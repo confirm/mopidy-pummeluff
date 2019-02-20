6a2c735ed80cd504480827a16241711f6f6d265c
Dominique Barton <dbarton@confirm.ch>
2019-02-20 23:18:20 +0100

    FEATURE: Add frontend with new RFID library which supports IRQ's

---
+++ mopidy_pummeluff/frontend.py
@@ -0,0 +1,81 @@
+# -*- coding: utf-8 -*-
+'''
+Python module for Mopidy Pummeluff frontend.
+'''
+
+from __future__ import absolute_import, unicode_literals, print_function
+
+from threading import Thread, Event
+from time import time
+from logging import getLogger
+
+import pykka
+from mopidy import core
+
+from .rfid_reader import RFIDReader, ReadError
+
+LOGGER = getLogger(__name__)
+
+
+class CardReader(Thread):
+    '''
+    Thread class which reads RFID cards from the RFID reader.
+    '''
+    def __init__(self, stop_event):
+        '''
+        Class constructor.
+
+        :param threading.Event stop_event: The stop event
+        '''
+        super(CardReader, self).__init__()
+        self.stop_event = stop_event
+
+    def run(self):
+        '''
+        Run RFID reading loop.
+        '''
+        reader    = RFIDReader()
+        prev_time = time()
+        prev_uid  = ''
+
+        while not self.stop_event.is_set():
+            reader.wait_for_tag()
+
+            try:
+                now = time()
+                uid = reader.uid
+
+                if now - prev_time > 1 or uid != prev_uid:
+                    LOGGER.info('Card with UID %s read', uid)
+
+                prev_uid  = uid
+                prev_time = now
+
+            except ReadError:
+                pass
+
+        reader.cleanup()
+
+
+class Frontend(pykka.ThreadingActor, core.CoreListener):
+    '''
+    Pummeluff frontend which basically reads cards from the RFID reader.
+    '''
+
+    def __init__(self, config, core):
+        super(Frontend, self).__init__()
+        self.core        = core
+        self.stop_event  = Event()
+        self.card_reader = CardReader(self.stop_event)
+
+    def on_start(self):
+        '''
+        Start card reader thread after the actor is started.
+        '''
+        self.card_reader.start()
+
+    def on_stop(self):
+        '''
+        Stop card reader thread before the actor stops.
+        '''
+        self.stop_event.set()
---
+++ mopidy_pummeluff/rfid_reader.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+'''
+Python card reader module.
+'''
+
+from __future__ import absolute_import, unicode_literals, print_function
+
+from RPi.GPIO import cleanup
+from pirc522 import RFID
+
+
+class ReadError(Exception):
+    '''
+    Exception which is thrown when the UID could not be read from the card.
+    '''
+
+
+class RFIDReader(RFID):
+    '''
+    Card reader for the RC522 RFID card reader board, based on the excellent
+    :py:class:`pirc522.RFID` class.
+    '''
+
+    @property
+    def uid(self):
+        '''
+        Return the UID from the card.
+
+        :return: The hex UID
+        :rtype: string
+        '''
+        error, data = self.request()
+        if error:
+            raise ReadError('Could not read tag')
+
+        error, uid_chunks = self.anticoll()
+        if error:
+            raise ReadError('Could not read UID')
+
+        uid = '{0[0]:02X}{0[1]:02X}{0[2]:02X}{0[3]:02X}'.format(uid_chunks)
+        return uid
+
+    def cleanup(self):
+        '''
+        Cleanup GPIO ports.
+        '''
+        cleanup()
